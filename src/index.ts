b23bb18f97d096df040aefde90b4bf6555aa8fc1
phuoctmse <phuoctmse184067@fpt.edu.vn>
2024-12-16 19:27:44 +0700

    testing prettier

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-const sum = (obj: {a: number; b: number}) => {
+const sum = (obj: { a: number; b: number }) => {
   return obj.a + obj.b
 }
 
 app.get('/', (req, res) => {
-  const total = sum({a:1, b:2})
+  const total = sum({ a: 1, b: 2 })
   res.send(`total : ${total}`)
 })
 
