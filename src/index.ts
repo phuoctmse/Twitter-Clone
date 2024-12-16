c00814069cc7cd6353cb767de6b8fa64d7abda8a
phuoctmse <phuoctmse184067@fpt.edu.vn>
2024-12-16 19:26:51 +0700

    config debug

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-const userName: string = 'Thomas Truong'
+import express from 'express'
+const app = express()
+const port = 3000
 
-console.log(userName)
+const sum = (obj: {a: number; b: number}) => {
+  return obj.a + obj.b
+}
+
+app.get('/', (req, res) => {
+  const total = sum({a:1, b:2})
+  res.send(`total : ${total}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
